b37f0f0a26158d871cf9e625f7be27f0820c17d2
Meet Patel <meetpatel@Meets-MBP-2.Optilink>
2023-05-11 10:39:21 +0530

    setup uniswap address and abi

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-import { BigNumber } from 'ethers';
+import { ethers, BigNumber } from 'ethers';
 import { ETH, USDC } from './tokens';
+import Web3 from 'web3';
+import * as fs from 'fs';
 
 const FROM_TOKEN = USDC;
 const FROM_BALANCE = BigNumber.from('1000000');
@@ -7,8 +9,14 @@ const TO_TOKEN = ETH;
 
 (async () => {
   console.info(`Converting ${FROM_BALANCE.toString()} ${FROM_TOKEN.symbol} to ${TO_TOKEN.symbol}`);
+  console.info('HELLO WORLD')
 
   // Get the contract for a DEX.
+  
+  const uniswapAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+  const jsonData = fs.readFileSync('abi.json', 'utf-8');
+  const uniswapABI = JSON.parse(jsonData);
+
 
   // Use ethers and the DEX contract to figure out how much TO_TOKEN you can get
   // for the FROM_TOKEN.
---
+++ tokens.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WETH = exports.ETH = exports.USDC = void 0;
+exports.USDC = {
+    name: 'USD Coin',
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    symbol: 'USDC',
+    decimals: 6,
+    coingecko: 'usd-coin',
+    coinmarketcap: 'usd-coin'
+};
+exports.ETH = {
+    name: 'Ethereum',
+    address: '0x0000000000000000000000000000000000000000',
+    symbol: 'ETH',
+    decimals: 18,
+    coingecko: 'ethereum',
+    coinmarketcap: 'ethereum'
+};
+exports.WETH = {
+    name: 'Wrapped ETH',
+    address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    symbol: 'WETH',
+    decimals: 18,
+    coingecko: 'weth',
+    coinmarketcap: 'weth'
+};
